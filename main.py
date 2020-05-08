e991d35d626df6a86f2cb32cf076d9e2584b0b84
Ben Barron <44043673+benbarron@users.noreply.github.com>
2020-05-07 19:23:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import time
+import sys
 import pandas as pd
 import numpy as np
+
 
 class Network:
 	def __init__(self):
 		self.nodes = []
-	
+
 	def add_node(self, id=None):
-		if id is None: 
+		if id is None:
 			raise Exception('An id for the node is required')
 		self.nodes.append(Node(id))
 
@@ -23,15 +25,16 @@ class Network:
 				origin_node = self.nodes[i]
 			if self.nodes[i].id == destination_id:
 				destination_node = self.nodes[i]
-		if origin_node is None or destination_node is None: 
+		if origin_node is None or destination_node is None:
 			raise Exception('Could not find both origin and destination nodes')
 		origin_node.add_route(destination_node, cost)
+
 
 class Node:
 	def __init__(self, id):
 		self.routes = []
 		self.id = id
-		self.distance = float('inf')	
+		self.distance = float('inf')
 		self.pred = None
 
 	def __repr__(self):
@@ -39,6 +42,7 @@ class Node:
 
 	def add_route(self, destination, cost):
 		self.routes.append(Route(self, destination, cost))
+
 
 class Route:
 	def __init__(self, via, destination, cost):
@@ -48,8 +52,9 @@ class Route:
 
 	def __repr__(self):
 		return f'Route(From: {str(self.via.id)}, To: {str(self.destination.id)}, Cost: {str(self.cost)})'
- 
-class DijkstrasShortestPath: 
+
+
+class DijkstrasShortestPath:
 	def __init__(self, network):
 		self.perm_nodes = []
 		self.temp_nodes = network.nodes.copy()
@@ -60,74 +65,87 @@ class DijkstrasShortestPath:
 	def run(self):
 		start_time = time.time()
 		while len(self.temp_nodes) > 0:
-			d = [n.distance for n in self.temp_nodes] ## find distances of nodes
-			m = self.temp_nodes[d.index(min(d))] ## get node with min distance
+			d = [n.distance for n in self.temp_nodes]
+			m = self.temp_nodes[d.index(min(d))]  # get node with min distance
 			for i in range(len(m.routes)):
-				if m.routes[i].destination.distance > m.routes[i].via.distance + m.routes[i].cost: 
-					m.routes[i].destination.distance = m.routes[i].via.distance + m.routes[i].cost
+				if m.routes[i].destination.distance > m.routes[i].via.distance + m.routes[i].cost:
+					m.routes[i].destination.distance = m.routes[i].via.distance + \
+						m.routes[i].cost
 					m.routes[i].destination.pred = m.routes[i].via
 			self.perm_nodes.append(m)
 			self.temp_nodes.remove(m)
-			# self.temp_nodes.pop(self.temp_nodes.index(m))
 		self.run_time = time.time() - start_time
 		self.run_finished = True
 
 	def print_summary(self):
-		if not self.run_finished: 
+		if not self.run_finished:
 			raise Exception('Run the algorithm before viewing the results.')
 
-		output = f'Dijkstra Finished in {self.run_time}\n\n'	
+		output = f'Dijkstra Finished in {self.run_time}\n\n'
 		output += 'N = Node\nD = Total Cost/Distance\nV = Via/Pred\n\n'
 		output += 'N\tD\tV\n'
 		output += '-\t-\t-\n'
-		
+
 		for i in range(len(self.perm_nodes)):
 			output += f'{self.perm_nodes[i].id}\t{self.perm_nodes[i].distance}\t'
 			if self.perm_nodes[i].pred is not None:
 				output += str(self.perm_nodes[i].pred.id)
-			else: 
+			else:
 				output += 'None'
 			output += '\n'
 		print(output)
+	
+	def prompt(self):
+		while 1:
+			a = input("Enter id of node to display shortest path to or 'quit' to exit: ")
+			if a.lower() == 'quit':
+				break
+			try:
+				self.print_path_to(node_id=a)
+			except Exception as e:
+				print(e)
+
 
 	def get_path_to(self, node=None, node_id=None):
-		if not self.run_finished: 
+		if not self.run_finished:
 			raise Exception('Run the algorithm before viewing the results.')
 		if node is None and node_id is None:
 			raise Exception('Either a node or node id is required')
 
 		if node is None:
-			ids = [node.id for node in self.perm_nodes] 
+			ids = [node.id for node in self.perm_nodes]
 			temp = self.perm_nodes[ids.index(node_id)]
-		else: 
-			temp = node 
-		
+		else:
+			temp = node
+
 		path = ''
-		while temp is not None: 
+		while temp is not None:
 			path = str(temp.id) + path
 			temp = temp.pred
 		return '-'.join(list(path))
-		
+
 	def print_path_to(self, node=None, node_id=None):
 		print(' -> '.join(self.get_path_to(node=node, node_id=node_id).split('-')))
 
+
 def main():
-	FILE_PATH = './data.csv'
-	data = pd.read_csv(FILE_PATH)
+	if len(sys.argv) < 2:
+		raise Exception('Please provide a filename as an argument')
+
+	data = pd.read_csv(sys.argv[1])
 	network = Network()
 
 	for _id in pd.unique(np.append(data['origin_id'].values, data['destination_id'].values)):
 		network.add_node(id=str(_id))
 
 	for i in range(data.shape[0]):
-		network.add_route(origin_id=str(data.iloc[i,0]), destination_id=str(data.iloc[i,1]), cost=data.iloc[i,2])
+		network.add_route(origin_id=str(data.iloc[i, 0]), destination_id=str(
+			data.iloc[i, 1]), cost=data.iloc[i, 2])
 
 	dijkstra = DijkstrasShortestPath(network)
-	dijkstra.run()                          
-	dijkstra.print_summary()                
-	dijkstra.print_path_to(node_id='e')       
-	
+	dijkstra.run()
+	dijkstra.print_summary()
+	dijkstra.prompt()
+
 if __name__ == '__main__':
 	main()
-
-
