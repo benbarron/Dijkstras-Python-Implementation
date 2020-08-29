6d04ae8ffe56a903c80fe3a7ef4f505737ff34ab
Ben Barron <bjbarron@uark.edu>
2020-08-28 19:16:57 -0500

    added helper functions to reduce code duplication

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ class Network:
 	def __init__(self):
 		self.nodes = []
 
-	def add_node(self, id=None):
-		if id is None:
-			raise Exception('An id for the node is required')
-		self.nodes.append(Node(id))
+	def find_nodes(self, origin_id, destination_id):
+		origin_node, destination_node = None, None
 
-	def add_route(self, origin_id=None, destination_id=None, cost=None):
-		if origin_id is None or destination_id is None:
-			raise Exception('origin_id and destination_id is required')
-		if cost is None:
-			cost = 0
-		origin_node = None
-		destination_node = None
 		for i in range(len(self.nodes)):
 			if self.nodes[i].id == origin_id:
 				origin_node = self.nodes[i]
 			if self.nodes[i].id == destination_id:
 				destination_node = self.nodes[i]
+
 		if origin_node is None or destination_node is None:
 			raise Exception('Could not find origin or destination node')
-		origin_node.add_route(destination_node, cost)
 
+		return origin_node, destination_node
+
+	def add_node(self, id=None):
+		self.nodes.append(Node(id))
+
+	def add_route(self, origin_id=None, destination_id=None, cost=0):
+		if origin_id is None or destination_id is None:
+			raise Exception('origin_id and destination_id is required')
+		origin_node, destination_node = self.find_nodes(origin_id, destination_id)
+		origin_node.add_route(destination_node, cost)
 
 class Node:
 	def __init__(self, id):
@@ -54,14 +55,72 @@ class Route:
 	def __repr__(self):
 		return f'Route(From: {str(self.via.id)}, To: {str(self.destination.id)}, Cost: {str(self.cost)})'
 
-
-class DijkstrasShortestPath:
+class Algorithm:
 	def __init__(self, network):
-		self.perm_nodes = []
-		self.temp_nodes = network.nodes.copy()
-		self.temp_nodes[0].distance = 0
 		self.run_time = 0
 		self.run_finished = False
+		self.perm_nodes = []
+		self.temp_nodes = network.nodes.copy()
+
+	def find_node(self, node_id):
+		ids = [node.id for node in self.perm_nodes]
+		return self.perm_nodes[ids.index(node_id)]
+
+	def get_path_to(self, node_id):
+		if not self.run_finished:
+			raise Exception('Run the algorithm before viewing the results.')
+
+		temp, path = self.find_node(node_id), ''
+
+		while temp is not None:
+			path = str(temp.id) + path
+			temp = temp.pred
+
+		return '-'.join(list(path))
+
+	def print_path_to(self, node_id):
+		print('Path: ' + ' -> '.join(self.get_path_to(node_id).split('-')))
+
+	def print_distance_to(self, node_id):
+		if not self.run_finished:
+			raise Exception('Run the algorithm before viewing the results.')
+
+		if (node := self.find_node(node_id)) is None:
+			raise Exception(f'Node with id {node_id} not found')
+
+		print(f'Distance: {node.distance}')
+
+	def print_summary(self):
+		if not self.run_finished:
+			raise Exception('Run the algorithm before viewing the results.')
+
+		output = f'\nDijkstra finished in {self.run_time} seconds\n\n' 
+		output += 'N = Node\nD = Total Cost/Distance\nV = Via/Pred\n\n' 
+		output += 'N\tD\tV\n-\t-\t-\n'
+
+		for i in range(len(self.perm_nodes)):
+			output += f'{self.perm_nodes[i].id}\t{self.perm_nodes[i].distance}\t'
+			if self.perm_nodes[i].pred is not None:
+				output += str(self.perm_nodes[i].pred.id)
+			else:
+				output += 'None'
+			output += '\n'
+		print(output)
+	
+	def prompt(self):
+		while 1:
+			if (a := input("Enter id of node to display shortest path to or 'quit' to exit: ").lower()) == 'quit':
+				break
+			try:
+				self.print_path_to(a)
+				self.print_distance_to(a)
+			except Exception as e:
+				print(e)
+
+class DijkstrasShortestPath(Algorithm):
+	def __init__(self, network):
+		Algorithm.__init__(self, network)
+		self.temp_nodes[0].distance = 0
 
 	def run(self, verbose=False):
 		start_time = time.time()
@@ -79,66 +138,10 @@ class DijkstrasShortestPath:
 		self.run_time = time.time() - start_time
 		self.run_finished = True
 
-	def print_summary(self):
-		if not self.run_finished:
-			raise Exception('Run the algorithm before viewing the results.')
-
-		output = f'\nDijkstra finished in {self.run_time} seconds\n\n'
-		output += 'N = Node\nD = Total Cost/Distance\nV = Via/Pred\n\n'
-		output += 'N\tD\tV\n'
-		output += '-\t-\t-\n'
-
-		for i in range(len(self.perm_nodes)):
-			output += f'{self.perm_nodes[i].id}\t{self.perm_nodes[i].distance}\t'
-			if self.perm_nodes[i].pred is not None:
-				output += str(self.perm_nodes[i].pred.id)
-			else:
-				output += 'None'
-			output += '\n'
-		print(output)
-	
-	def prompt(self):
-		while 1:
-			if (a := input("Enter id of node to display shortest path to or 'quit' to exit: ").lower()) == 'quit':
-				break
-			try:
-				self.print_path_to(node_id=a)
-				self.print_distance_to(node_id=a)
-			except Exception as e:
-				print(e)
-
-
-	def get_path_to(self, node_id=None):
-		if not self.run_finished:
-			raise Exception('Run the algorithm before viewing the results.')
-		if node_id is None:
-			raise Exception('A node_id id is required')
-
-		ids = [node.id for node in self.perm_nodes]
-		temp = self.perm_nodes[ids.index(node_id)]
-
-		path = ''
-		while temp is not None:
-			path = str(temp.id) + path
-			temp = temp.pred
-		return '-'.join(list(path))
-
-	def print_path_to(self, node_id=None):
-		print('Path: ' + ' -> '.join(self.get_path_to(node_id=node_id).split('-')))
-
-	def print_distance_to(self, node_id=None):
-		if not self.run_finished:
-			raise Exception('Run the algorithm before viewing the results.')
-		if node_id is None:
-			raise Exception('A node_id is required')	
-
-		ids = [node.id for node in self.perm_nodes]
-		node = self.perm_nodes[ids.index(node_id)]
-
-		if node is None:
-			raise Exception(f'Node with id {node_id} not found')
-
-		print(f'Distance: {node.distance}')
+class AStar(Algorithm):
+	def __init__(self, network):
+		Algorithm.__init__(self, network)
+		## todo
 
 def check_file_input(file_path):
 	path_arr = list(file_path)
